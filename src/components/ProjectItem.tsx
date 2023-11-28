37a0a74ce66e94ec2595dfa9448d9e9b4d626ed4
Curtis Wong <cwong990@berkeley.edu>
2023-11-28 09:32:46 -0800

    update doelger image

--- src/components/ProjectItem.tsx
+++ src/components/ProjectItem.tsx
@@ -1,30 +1,37 @@
-import React from 'react';
-import Link from 'next/link';
-import Image, { StaticImageData } from 'next/image';
-import { Url } from 'next/dist/shared/lib/router/router';
+import React from "react";
+import Link from "next/link";
+import Image, { StaticImageData } from "next/image";
+import { Url } from "next/dist/shared/lib/router/router";
 
 interface Source {
-    title: String,
-    imagebg: StaticImageData, 
-    tech: String,
-    url: string
+  title: String;
+  imagebg: StaticImageData;
+  tech: String;
+  url: string;
 }
 
-const ProjectItem = ({title, imagebg, tech, url}: Source) => {
+const ProjectItem = ({ title, imagebg, tech, url }: Source) => {
+  // const urlString = new URL(url);
 
-    // const urlString = new URL(url);
-    
-    return (
-        <div className="relative flex items-center justify-center h-auto w-full shadow-xl shadow-gray-400 rounded-xl p-4 group hover:bg-gradient-to-r from-[#5651e5] to-[#709dff]">
-        <Image className="rounded-xl group-hover:opacity-10" src={imagebg} alt="/"/>
-        <div className="hidden group-hover:block absolute top-[50%] left-[50%] translate-x-[-50%] translate-y-[-50%]">
-            <h3 className="text-2xl text-white tracking-wider text-center">{title}</h3>
-            <p className="pb-4 pt-2 text-white text-center">{tech}</p>
-            <Link href={url}>
-                <p className="text-center py-3 rounded-lg bg-white text-gray-700 font-bold text-lg cursor-pointer">Learn More</p>
-            </Link>
-        </div>
-        </div>
-    )
-}
+  return (
+    <div className="relative flex items-center justify-center h-auto w-full shadow-xl shadow-gray-400 rounded-xl p-4 group hover:bg-gradient-to-r from-[#5651e5] to-[#709dff]">
+      <Image
+        className="rounded-xl group-hover:opacity-10"
+        src={imagebg}
+        alt="/"
+      />
+      <div className="hidden group-hover:block absolute top-[50%] left-[50%] translate-x-[-50%] translate-y-[-50%]">
+        <h3 className="text-2xl text-white tracking-wider text-center">
+          {title}
+        </h3>
+        <p className="pb-4 pt-2 text-white text-center">{tech}</p>
+        <Link href={url}>
+          <p className="text-center py-3 rounded-lg bg-white text-gray-700 font-bold text-lg cursor-pointer">
+            Learn More
+          </p>
+        </Link>
+      </div>
+    </div>
+  );
+};
 export default ProjectItem;
